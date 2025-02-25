912ee734d0ce324861f2da728044fc149c781b44
Preethi Gnanasekhar <preethignanasekhar444@gmail.com>
2025-02-25 21:28:15 +0530

    Add files via upload

---
+++ start and end.java.java
@@ -0,0 +1,16 @@
+
+//num between start and end
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int start=s.nextInt();
+	    int end=s.nextInt();
+	    for(int i=start;i<=end;i++){
+	     System.out.println(i);   
+	    }
+	
+	}
+}
